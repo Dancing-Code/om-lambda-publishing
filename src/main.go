7c26facab74829aa96b15c70c5bf037e775302f1
behike56 <living.tech.dancing.code@gmail.com>
2021-10-31 20:49:28 +0900

    feat

--- src/main.go
+++ src/main.go
@@ -5,13 +5,20 @@ import (
     "net/http"
     "os"
     "strconv"
+    "html/template"
 )
 
 /* 開始起点  */
 func main() {
+    // マルチプレクサの生成
     mux := http.NewServeMux()
+
+    // マルチプレクサで静的なファイルの返送にも使える
     files := http.FileServer(http.Dir("/public"))
+    // URLパスからプレフィックスを削除するにはStripPrefixを使う。
     mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+    // ルートURLをハンドラ関数にリダイレクト
     mux.HandleFunc("/", index)
 
     server := &http.Server{
@@ -20,4 +27,14 @@ func main() {
     }
 
     server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    files := []string("templates/layout.html",
+        "templates/navbar.html",
+        "templates/index.html",)
+    templates := template.Must(template.ParseFiles(files...))
+    threads, err := data.Threads(); if err == ninl {
+        templates.ExcuteTemplate(w, "layout", threads)
+    }
 }
